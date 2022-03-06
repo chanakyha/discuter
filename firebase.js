2d710f2d85991e69286b8e59c72fbfd4a3e9b42e
Chanakyha <66877639+chanakyha@users.noreply.github.com>
2022-03-06 18:58:24 +0530

    React Native Navigation Module is installed

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBEibiq1yYcEhWaUSMqNVkGZR6wPmtohNE",
+  authDomain: "discuter-da294.firebaseapp.com",
+  projectId: "discuter-da294",
+  storageBucket: "discuter-da294.appspot.com",
+  messagingSenderId: "134081931107",
+  appId: "1:134081931107:web:bfe2d569534ca5051e5556",
+};
